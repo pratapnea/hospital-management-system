6874ba7dcd65896a9c69413334e37f5baedf8e37
pratapnea <sereversion@gmail.com>
2021-08-29 14:56:25 +0545

    patient service created

--- src/main/java/com/example/hms/exception/PatientNotFoundException.java
+++ src/main/java/com/example/hms/exception/PatientNotFoundException.java
@@ -4,14 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class PatientNotFoundExcepton extends RuntimeException {
+public class PatientNotFoundException extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public PatientNotFoundExcepton(String message) {
+	public PatientNotFoundException(String message) {
 		super(message);
 	}
 	
---
+++ src/main/java/com/example/hms/service/PatientService.java
@@ -0,0 +1,65 @@
+package com.example.hms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.hms.exception.PatientNotFoundException;
+import com.example.hms.model.Patient;
+import com.example.hms.repository.PatientRepository;
+
+@Service
+public class PatientService {
+	private PatientRepository patientRepository;
+	
+	@Autowired
+	public PatientService(PatientRepository patientRepository) {
+		this.patientRepository = patientRepository;
+	}
+	
+	// add patient
+	public Patient addPatient(Patient patient) {
+		return patientRepository.save(patient);
+	}
+	
+	// find all patient
+	public List<Patient> findAllPatient() {
+		return patientRepository.findAll();
+	}
+	
+	// find patient by id
+	public Patient findPatientById(Long id) {
+		return patientRepository.findById(id)
+				.orElseThrow(()-> new PatientNotFoundException("Patient Not Found!"));
+	}
+	
+	// update patient
+	public Patient updatePatient(Patient patient) {
+		return patientRepository.save(patient);
+	}
+	
+	// delete patient
+	public void deletePatient(Long id) {
+		patientRepository.deleteById(id);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

531cf18d1c94c4585ee82ec048d16772f71f80a3
pratapnea <sereversion@gmail.com>
2021-08-30 11:01:22 +0545

    updated controller and services

---
+++ src/main/java/com/example/hms/controller/MedicalPersonController.java
@@ -0,0 +1,5 @@
+package com.example.hms.controller;
+
+public class MedicalPersonController {
+
+}
--- src/main/java/com/example/hms/repository/PatientRepository.java
+++ src/main/java/com/example/hms/repository/PatientRepository.java
@@ -1,9 +1,11 @@
 package com.example.hms.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hms.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+	List<Patient> findByMedicalPersonId(Long medicalPersonId);
 }

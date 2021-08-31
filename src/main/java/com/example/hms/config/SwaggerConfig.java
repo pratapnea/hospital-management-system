2ac34c7ead4170326c0a434dfd7ce7bea72d0e66
pratapnea <sereversion@gmail.com>
2021-08-31 19:36:11 +0545

    added swagger for project

---
+++ src/main/java/com/example/hms/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.hms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.annotations.Api;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(Api.class)).paths(PathSelectors.any()).build()
+				.pathMapping("/").apiInfo(apiInfo()).useDefaultResponseMessages(false);
+	}
+	
+	public ApiInfo apiInfo() {
+		final ApiInfoBuilder builder = new ApiInfoBuilder();
+		builder.title("Hospital Management System APIs").version("1.0").license("© Copyright 2021")
+				.description("The API provides endpoints for Hospital Management System");
+		return builder.build();
+	}
+}
